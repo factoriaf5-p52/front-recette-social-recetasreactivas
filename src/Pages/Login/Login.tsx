674e08a218ed893186c0a07a1a4a1899013b0e8c
gbixio <galbixio93@gmail.com>
2023-02-27 13:12:57 +0100

    login page done

--- src/Pages/Login/Login.tsx
+++ src/Pages/Login/Login.tsx
@@ -1,20 +1,32 @@
 import { Button, Input } from '../../components/elements/index'
-import { Form, LoginStyle } from './loginStyle'
+import { Form, ImageCover, LogInNav, LoginStyle } from './loginStyle'
+import imgPan from '../../assets/imgPan.svg'
+import logoIcon from '../../assets/logoIcon.svg'
+import backArrow from '../../assets/backArrow.svg'
 
 const Login = () =>  {
 
     return (
     <>
     <LoginStyle>
-        
+        <LogInNav>
+            <img src={backArrow} alt="back" />
+            <h3>Login</h3>
+        </LogInNav>
+        <ImageCover>
+            <img src={imgPan} alt="someone cooking illustration" />
+        </ImageCover>
 
         <Form>
             <Input  placeholder="Enter your username"/>
             <Input  placeholder="Password"/>
            <Button label="Sign In" />
         </Form>
-    </LoginStyle>
 
+<footer>        
+    <img src={logoIcon} alt="logo" />
+</footer>
+    </LoginStyle>
     </>
     )
     
--- src/Pages/Login/loginStyle.tsx
+++ src/Pages/Login/loginStyle.tsx
@@ -1,13 +1,50 @@
 import styled from "styled-components";
 
 export const LoginStyle = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+height: 100vh;
+
+footer {
+position: absolute;
+bottom: 0;
+}
+`
+export const LogInNav = styled.div`
+position: absolute;
+top: 0;
+display: flex;
+justify-content: flex-start;
+align-items: center;
+width: 90%;
+height: 80px;
+color: black;
+margin: 1.5em 0 0 2em;
+
+h3 {
+margin-left: 28%;
+}
+`
+export const ImageCover = styled.div`
+position: absolute;
+text-align: center;
+width: 308px;
+height: 252px;
+left: 34px;
+top: 119px;
+background: #C4E2FF;
+border-radius: 40px;
 
 `
 export const Form = styled.div`
+position: relative;
 display: flex;
 flex-direction: column;
 gap: 20px;
 align-items: center;
-margin-top: 3rem;
+top: 119px;
 
 `
+
