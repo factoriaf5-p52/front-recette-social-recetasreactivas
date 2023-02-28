cc8f954765af6563c568012ca57c6f8f6473bf1c
Sergi <sergialsina81@gmail.com>
2023-02-28 16:21:21 +0100

    Uploadform page and styles

--- src/Pages/UploadForm/UploadForm.tsx
+++ src/Pages/UploadForm/UploadForm.tsx
@@ -1,10 +1,35 @@
-import { UploadFormStyle } from './UploadFormStyle'
+import { Button, Input } from '../../components/elements/index'
+import { Form, ImageCover, UploadNav, UploadFormStyle } from './UploadFormStyle'
+import imgList from '../../assets/imgList.svg'
+import logoIcon from '../../assets/logoIcon.svg'
+import backArrow from '../../assets/backArrow.svg'
+
 
 const UploadForm = () =>  {
 
     return (
     <>
-    <UploadFormStyle>Uploeadederete</UploadFormStyle>
+    <UploadFormStyle>
+    <UploadNav>
+            <img src={backArrow} alt="back" />
+            <h3>Form</h3>
+        </UploadNav>
+        <ImageCover>
+            <img src={imgList} alt="someone cooking illustration" />
+        </ImageCover>
+
+        <Form>
+            <Input  placeholder="Recipe name"/>
+            <Input  placeholder="Instructions"/>
+            <Input  placeholder="Ingredients"/>
+            <Input  placeholder="Add Image"/>
+           <Button label="Send" />
+        </Form>
+
+<footer>        
+    <img src={logoIcon} alt="logo" />
+</footer>
+    </UploadFormStyle>
     </>
     )
     
